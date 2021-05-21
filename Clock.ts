b5ffb60f942dc24a12a733e0ae6f238601a4b273
longdog <linda@longdog.us>
2021-05-21 13:00:48 +0400

    add Clock component

---
+++ Clock.ts
@@ -0,0 +1,31 @@
+import { LitElement, html } from "lit";
+import { customElement, property, state } from "lit/decorators.js";
+
+@customElement("clock-elem")
+class Clock extends LitElement {
+  @state()
+  private date = new Date();
+
+  private timerID: number;
+
+  tick() {
+    this.date = new Date();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    return html` <div>
+      <h1>Hello, world!</h1>
+      <h2>It is ${this.date.toLocaleTimeString()}.</h2>
+    </div>`;
+  }
+}
