28e245ced43f95eecad81012dc062080c5009368
Denis Abrosimov <denis.abrosimov@gmail.com>
2021-05-21 10:06:43 +0400

    web components hello word

---
+++ index.js
@@ -0,0 +1,5 @@
+import { html, render } from "lit";
+const name = "Зюзя";
+const elem = html `<div>hello ${name}</div>`;
+render(elem, document.getElementById("root"));
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,6 @@
+import { html, render } from "lit";
+
+const name = "Зюзя";
+const elem = html`<div>hello ${name}</div>`;
+
+render(elem, document.getElementById("root"));
